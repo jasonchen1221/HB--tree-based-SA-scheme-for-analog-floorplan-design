579200c83fb9c49b12c25e69e54e3e015074dd11
jasonchen1221 <jasonchen811221@gmail.com>
2022-11-12 19:40:18 +0800

    feature: class Net

---
+++ src/net.cpp
@@ -0,0 +1,39 @@
+#include "net.hpp"
+
+Net::Net(){
+    m_vBlks.resize(0);
+    m_vPins.resize(0);
+}
+  
+/* member functions */ 
+void Net::addBlk(Block b){
+    m_vBlks.push_back(b);
+}
+    
+void Net::addPin(Pin p){
+    m_vPins.push_back(p);
+
+    minX = std::min(minX, p.getX());
+    maxX = std::max(maxX, p.getX());
+    minY = std::min(minY, p.getY());
+    maxY = std::max(maxY, p.getY());
+}
+
+double Net::getThisNetHPWL() {    
+    return ((maxX - minX) + (maxY - minY));
+}
+/* info display & check */
+std::ostream& operator<<(std::ostream& os, const Net& net){
+    std::cout << "This net's degree: " << net.m_vBlks.size() << std::endl;
+    // display connected blks
+    std::cout << "connected blks:" << std::endl;
+    for(int i = 0; i < net.m_vBlks.size(); ++i){
+        os << net.m_vBlks[i].getName() << std::endl;
+    }
+    // display connected pins
+    std::cout << "connected pins:" << std::endl;
+    for(int i = 0; i < net.m_vPins.size(); ++i){
+        os << net.m_vPins[i].getName() << std::endl;
+    }
+    return os;
+}
---
+++ src/net.hpp
@@ -0,0 +1,32 @@
+#ifndef NET_HPP
+#define NET_HPP
+
+#include "block.hpp"
+#include "pin.hpp"
+#include <limits>
+
+class Net{
+    friend class FloorPlanner;
+
+public:
+    Net();
+    ~Net() {};
+    
+    /* member functions */ 
+    void addBlk(Block b);
+    void addPin(Pin p);
+    double getThisNetHPWL();
+
+    /* info display & check */
+    friend std::ostream& operator<<(std::ostream& os, const Net& net);
+
+private:
+    std::vector<Block> m_vBlks;
+    std::vector<Pin>   m_vPins;
+    double minX = std::numeric_limits<double>::max();
+    double maxX = std::numeric_limits<double>::lowest();
+    double minY = std::numeric_limits<double>::max();
+    double maxY = std::numeric_limits<double>::lowest();
+};
+
+#endif  //NET_HPP
