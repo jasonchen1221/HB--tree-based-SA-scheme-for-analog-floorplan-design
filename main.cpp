ee706ec11827098b4a9bb00fe8776f650f0d6eb0
jasonchen1221 <jasonchen811221@gmail.com>
2022-12-09 15:02:15 +0800

    pybind wrapup

--- main.cpp
+++ main.cpp
@@ -1,122 +1,37 @@
-#include <iostream>
-#include "placer.hpp"
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <pybind11/attr.h>
+#include "headers/placer.hpp"
 
-using std::cout;
-using std::endl;
+namespace py = pybind11;
+
 time_t startTime;
 
-int main(int argc, char* argv[]){
- 
-    startTime = clock();
+PYBIND11_MODULE(_Placer, m){
+    m.doc() = "pybind11 plugin";
+    py::class_<Block>(m, "Block")
+        .def(py::init())
+        .def("setName", &Block::setName)
+        .def("setW",    &Block::setW)
+        .def("setH",    &Block::setH)
+        .def("getX",    &Block::getX)
+        .def("getY",    &Block::getY);
     
-    Placer placer;
-    // alpha, blkFile, netFile, outputFile
-    placer.parseAll(argv[1], argv[2], argv[3]);     
+    py::class_<BTree>(m, "BTree")
+        .def(py::init<size_t>())
+        .def("reset",            &BTree::reset)
+        .def("insertT",          &BTree::insertT)
+        .def("printTree",        &BTree::printTree)
+        .def("printList",        &BTree::printList)
+        .def("packing",          &BTree::packing)
+        .def("printContouLines", &BTree::printContourLines)
+        .def("generalSwap",      &BTree::generalSwap)
+        .def("connectSwap",      &BTree::connectSwap)
+        .def("sameParentSwap",   &BTree::sameParentSwap)
+        .def("leftRotateB",      &BTree::leftRotateB)
+        .def("rightRotateB",     &BTree::rightRotateB)
+        .def("dumpCaseResult",   &BTree::dumpCaseResult)
+        ;
 
-    placer.place();
-
-    placer.writeResult(argv[4]);
-
-    time_t endTime = clock();
-    double t_used = (double)(endTime - startTime) / CLOCKS_PER_SEC;
-    cout << ">> Running Time: " << t_used << " sec." << endl;
-
-
-/*
-    // Testing BTree and Contourlines
-    BTree btree;
-    btree.reset();
-    //btree.printTree();
-    //btree.printList();
-
-    Block* blk_1 = new Block; blk_1->setName("b1"); blk_1->setW(5); blk_1->setH(3);
-    Block* blk_2 = new Block; blk_2->setName("b2"); blk_2->setW(7); blk_2->setH(2);
-    Block* blk_3 = new Block; blk_3->setName("b3"); blk_3->setW(6); blk_3->setH(4);
-    Block* blk_4 = new Block; blk_4->setName("b4"); blk_4->setW(2); blk_4->setH(5);
-
-    btree.insertT(blk_1);
-    cout << "Inserting blk_1" << endl;
-    cout << "Print Tree: " << endl;
-    btree.printTree();
-    cout << "Print Tree List: " << endl;
-    btree.printList();
-    cout << blk_1->getX() << "," << blk_1->getY() << endl; 
-    
-    btree.insertT(blk_2);
-    cout << "Inserting blk_2" << endl;
-    cout << "Print Tree: " << endl;
-    btree.printTree();
-    cout << "Print Tree List: " << endl;
-    btree.printList();
-    cout << blk_2->getX() << "," << blk_2->getY() << endl; 
-
-    btree.insertT(blk_3);
-    cout << "Inserting blk_3" << endl;
-    cout << "Print Tree: " << endl;
-    btree.printTree();
-    cout << "Print Tree List: " << endl;
-    btree.printList();
-    cout << blk_3->getX() << "," << blk_3->getY() << endl; 
-
-    btree.insertT(blk_4);
-    cout << "Inserting blk_4" << endl;
-    cout << "Print Tree: " << endl;
-    btree.printTree();
-    cout << "Print Tree List: " << endl;
-    btree.printList();
-    cout << blk_4->getX() << "," << blk_4->getY() << endl; 
-
-    cout << "test root:" << endl;
-    cout << btree.getRoot()->getName() << " " 
-         << btree.getRoot()->getX() << " "
-         << btree.getRoot()->getW() << " "
-         << btree.getRoot()->getH() << endl;
-
-
-    // ContourLines will be set when packing
-    cout << "----------Packing----------" << endl;
-    btree.packing();
-    std::cout << "Print Tree ContourLines: " << std::endl;
-    btree.printContourLines();
-
-    cout << "b1: " << blk_1->getX() << "," << blk_1->getY() << endl; 
-    cout << "b2: " << blk_2->getX() << "," << blk_2->getY() << endl; 
-    cout << "b3: " << blk_3->getX() << "," << blk_3->getY() << endl; 
-    cout << "b4: " << blk_4->getX() << "," << blk_4->getY() << endl; 
-
-    // test: generalSwap
-    cout << "----------generalSwap----------" << endl;
-    btree.generalSwap(blk_1, blk_3);
-    cout << "Print Tree: " << endl;
-    btree.printTree();
-    cout << "Print Tree List: " << endl;
-    btree.printList();
-    //btree.packing();
-    //btree.printContourLines();
-    
-    // test: connectSwap
-    cout << "----------connectSwap----------" << endl;
-    btree.connectSwap(blk_3, blk_2);
-    cout << "Print Tree: " << endl;
-    btree.printTree();
-    cout << "Print Tree List: " << endl;
-    btree.printList();
-
-    // test: sameParentSwap
-    cout << "----------sameParentSwap----------" << endl;
-    btree.sameParentSwap(blk_4, blk_3);
-    cout << "Print Tree: " << endl;
-    btree.printTree();
-    cout << "Print Tree List: " << endl;
-    btree.printList();
-
-    // test: leftRotateB
-
-    // test: rightRotateB
-
-
-    vector<Block*> testVec{ blk_1, blk_2, blk_3, blk_4 };
-    btree.dumpCaseResult(testVec);
-*/
-    return 0;
+    ;
 }
